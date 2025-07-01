4ff8881e99870032c00012d7f36cf800fc68ec84
Gustavo D. Jimenez Mora <gusweb71@gmail.com>
2025-07-01 15:59:03 -0500

    Clase 06 useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { Button } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("Gustavo");
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
-  /*
-  Batching is a feature in React that allows multiple state updates to be grouped together,
-  so that the component only re-renders once, instead of multiple times for each state update
-  This is particularly useful for performance optimization, as it reduces the number of re-renders
-  and improves the overall performance of the application.
-  
-  const countMore = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+
+      if (!response.ok) {
+        throw new Error("Error al obtener los datos");
+      }
+
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (err) {
+      setError(err as string);
+    } finally {
+      setLoading(false);
+    }
   };
 
-*/
-  const countMore = () => {
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-  const changeName = () => {
-    setName("Daniel");
-  };
-
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+  if (error) {
+    return <div>Ups! Hay un error: {error}</div>;
+  }
   return (
     <>
-      <Button label={`Count is ${count}`} parentMethod={countMore} />
-      <p> {name} </p>
-      <Button label="cambiar nombre" parentMethod={changeName} />
+      <div> {JSON.stringify(data)} </div>
     </>
   );
 }
 
 export default App;
-// This is a simple React application that uses a Button component to display and increment a count.
-// The Button component is imported from the components directory, and it takes a label and a method
-// to be called when the button is clicked. The count state is managed using the useState hook, and the countMore function increments the count by 1 each time the button is clicked.
