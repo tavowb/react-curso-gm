65c39178de9784e0d47ea5bef18b7657fdc7e60c
Gustavo D. Jimenez Mora <gusweb71@gmail.com>
2025-07-09 15:02:49 -0500

    Clase 07: CustomHooks - useFetch, ejemplo alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import { useFetch } from "./hooks";
 const url = "https://jsonplaceholder.typicode.com/posts";
+//User ejemplo
+//const user = "https://jsonplaceholder.typicode.com/user";
 interface Data {
   name: string;
   lastname: string;
@@ -9,6 +11,8 @@ interface Data {
 
 function App() {
   const { data, loading, error } = useFetch<Data>(url);
+  //const { data: userData, loading: userLoading, error: userError } = useFetch<Data>(user); ejemplo del alias
+
   // useFetch es un hook personalizado que maneja la logica de la peticion
   // y devuelve los datos, el estado de carga y los errores
   // data es el resultado de la peticion, loading es un booleano que indica si
