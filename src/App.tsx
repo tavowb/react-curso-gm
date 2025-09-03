34047b930b0cff76ed0708213d9e1b36fc3ee816
Gustavo D. Jimenez Mora <gusweb71@gmail.com>
2025-09-03 16:14:15 -0500

    feat: Clase 10.1 ( UseRef) UseCallback UseMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
 import "./App.css";
-import { AppForm, Button, ColorRed } from "./components";
-import { GlobalProvider } from "./context/global.provider";
+import { BookReader, FocusInput } from "./components";
 
 function App() {
-  const handleClick = () => {
-    console.log("uy me clickio todo");
-  };
-
-  const submit = () => {
-    console.log("submitted");
-  };
-
-  const dimeHola = () => {
-    alert("hola !!");
-  };
-
   return (
-    <GlobalProvider>
-      <ColorRed>
-        <Button parentMethod={dimeHola}>My Boton Rojo</Button>
-      </ColorRed>
-      <Button parentMethod={handleClick}> My Boton Normal</Button>
-
-      <AppForm>
-        <button type="submit" onClick={submit}></button>
-      </AppForm>
-    </GlobalProvider>
+    <>
+      <BookReader />
+      <FocusInput />
+    </>
   );
 }
 
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,5 @@
-// eslint-disable-next-line react-refresh/only-export-components
+/* eslint-disable react-refresh/only-export-components */
+
 export * from "./Button/Button.tsx";
-// eslint-disable-next-line react-refresh/only-export-components
 export * from "./Form/Form.tsx";
+export * from "./UseRef";
