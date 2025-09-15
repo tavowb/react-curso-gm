a31f8a29ea1505769a41aa52c13d52f559eafdae
Gustavo D. Jimenez Mora <gusweb71@gmail.com>
2025-09-15 18:06:59 -0500

    feat: Clase 10.2  UseRef UseCallback (UseMemo)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.css";
-import { BookReader, FocusInput } from "./components";
+import { ShoppingCart } from "./components";
 
 function App() {
   return (
     <>
-      <BookReader />
-      <FocusInput />
+      {/*<BookReader />*/}
+      {/*<FocusInput />*/}
+      <ShoppingCart />
     </>
   );
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@
 export * from "./Button/Button.tsx";
 export * from "./Form/Form.tsx";
 export * from "./UseRef";
+export * from "./UseMemo/ShoppingCart.tsx";
