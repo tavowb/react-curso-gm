51c709316618bb91ef9540eb774f218e1899b114
Gustavo D. Jimenez Mora <gusweb71@gmail.com>
2025-10-02 17:53:33 -0500

    feat: Clase 12  error boundaries

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import "./App.css";
-import { Modal } from "./components";
-import { useModalContext } from "./components/Modal/context/";
+import { PromiseError } from "./components/ErrorBoundayExamples";
 
 function App() {
-  const { setState } = useModalContext();
-
-  const openModal = () => {
-    setState(true);
-  };
-
   return (
     <>
-      <Modal>
-        <h1>Hola Mimi</h1>
-        <h3>Te quiero</h3>
-      </Modal>
-      <button onClick={openModal}>Abrete sesamo</button>
+      <PromiseError />
     </>
   );
 }
